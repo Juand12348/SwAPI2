7ba348e9f007ffc3ab2b82f4810799e408cdbbf7
Juand12348 <156850435+Juand12348@users.noreply.github.com>
2025-10-15 01:29:02 +0000

    index-html

--- js/informativa.js
+++ js/informativa.js
@@ -1,15 +1,15 @@
 function Informativa() {
     const root = document.getElementById("root");
     root.innerHTML = `
-    <h1>SWA API</h1>
-    </br>
-    <p>Juan David Martínez Niño</p>
-    </br>
-    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg" atl="Logo Star Wars"/>
-    <div>
-    <p>Api con información de los 83 personajes de Star Wars</p>
-    </div>
-    <p>https://github.com/Juand12348</p>
-    <p>v 1.1.0</p>
+    <section class="informativa">
+        <h1 class="titulo">SWA API</h1>
+        <p class="autor">Juan David Martínez Niño</p>
+        <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg" alt="Logo Star Wars"/>
+        <div class="descripcion">
+            <p>API con información de los 83 personajes de Star Wars</p>
+        </div>
+        <p class="link"><a href="https://github.com/Juand12348" target="_blank">github.com/Juand12348</a></p>
+        <p class="version">v 1.1.0</p>
+    </section>
     `;
 }
